c50c9b278e06ff33fb6cb2bf8e23e8d658badd9d
sn8 <>
2018-07-15 11:12:38 +0800

    Change TradingView.php

--- TradingView.php
+++ TradingView.php
@@ -71,10 +71,9 @@ class TradingView extends Widget
 
         // prepare and register JavaScript code block
         $jsOptions = Json::encode($this->options);
-        $js = "TradingView.onready(function() { var widget = window.tvWidget = new TradingView.widget($jsOptions); });";
+        $js = "var widget = window.tvWidget = new TradingView.widget($jsOptions);";
         $key = __CLASS__ . '#' . $this->id;
         
         $this->view->registerJs($js, View::POS_READY, $key);
     }
-
 }
